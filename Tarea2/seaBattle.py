f20c1b6be1b29de29fe2768821a9e386e7bea2b5
Diego <diego88aht@gmail.com>
2026-02-19 20:38:57 -0400

    Metodo shoot() mejorado

--- Tarea2/seaBattle.py
+++ Tarea2/seaBattle.py
@@ -19,7 +19,13 @@ class SeaBattaleField:
     def shoot(self, x, y):
         if self.board[y][x] == self.SHIP:
             self.board[y][x] = self.HIT
+            
+            if self.is_ship_destroyed(x, y):
+                self.mark_ship_as_killed(x, y)
+                return 2
+
             return 1
+        
         else:
             self.board[y][x] = self.MISS
             return 0
@@ -40,6 +46,13 @@ class SeaBattaleField:
                 if self.can_place_ship(x, y, size, orientation):
                     self.place_ship(x, y, size, orientation)
                     placed = True
+
+    def is_loser(self):
+        for row in self.board:
+            if self.SHIP in row:
+                return False
+        
+        return True
 
     def can_place_ship(self, x, y, size, orientation):
         for i in range(size):
@@ -80,6 +93,52 @@ class SeaBattaleField:
                 ny = y + i
 
             self.board[ny][nx] = self.SHIP
+
+    def is_ship_destroyed(self, x, y):
+        visited = set()
+        stack = [(x, y)]
+
+        while stack:
+            (cx, cy) = stack.pop()
+
+            if (cx, cy) in visited:
+                continue
+            visited.add((cx, cy))
+
+            if self.board[cy][cx] == self.SHIP:
+                return False
+            
+            for (dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                (nx, ny) = cx + dx, cy + dy
+
+                if 0 <= nx < self.size and 0 <= ny < self.size:
+                    if self.board[ny][nx] in (self.SHIP, self.HIT):
+                        stack.append((nx, ny))
+
+        return True
+    
+    def mark_ship_as_killed(self, x, y):
+        visited = set()
+        stack = [(x, y)]
+        
+        while stack:
+            (cx, cy) = stack.pop()
+
+            if (cx, cy) in visited:
+                continue
+            visited.add((cx, cy))
+
+            if self.board[cy][cx] == self.HIT:
+                self.board[cy][cx] = self.KILL
+
+            for (dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                (nx, ny) = cx + dx, cy + dy
+
+                if 0 <= nx < self.size and 0 <= ny < self.size:
+                    if self.board[ny][nx] == self.HIT:
+                        stack.append((nx, ny))
+
+                        
 
 
 field = SeaBattaleField()
